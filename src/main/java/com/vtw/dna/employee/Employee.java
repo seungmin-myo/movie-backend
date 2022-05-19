964fad431f2f62f8fbe0bef8f999a8c1979edf4f
우태진 <taejin.woo@vtw.co.kr>
2022-05-19 19:05:46 +0900

    Initial Commit

---
+++ src/main/java/com/vtw/dna/employee/Employee.java
@@ -0,0 +1,28 @@
+package com.vtw.dna.employee;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Gender gender;
+    private LocalDate birthDate;
+
+    public Employee update(Employee newOne) {
+        this.name = newOne.name;
+        this.gender = newOne.gender;
+        this.birthDate = newOne.birthDate;
+        return this;
+    }
+}
---
+++ src/main/java/com/vtw/dna/employee/controller/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.vtw.dna.employee.controller;
+
+import com.vtw.dna.employee.Employee;
+import com.vtw.dna.employee.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    @GetMapping
+    public Page<Employee> list(@RequestParam("page") int page,
+                               @RequestParam("size") int size,
+                               @RequestParam(value = "sortColumn", defaultValue = "id") String sortColumn,
+                               @RequestParam(value = "sortDir", defaultValue = "asc") String sortDir,
+                               @RequestParam(value = "searchName", defaultValue = "") String searchName) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(sortDir), sortColumn));
+        Page<Employee> employees = repository.findAllByNameContains(pageable, searchName);
+        return employees;
+    }
+
+    @GetMapping("/{id}")
+    public Employee find(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow();
+        return employee;
+    }
+
+    @PostMapping
+    public Employee create(@RequestBody Employee newOne) {
+        repository.save(newOne);
+        return newOne;
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(@PathVariable Long id, @RequestBody Employee newOne) {
+        Employee oldOne = repository.findById(id).orElseThrow();
+        oldOne.update(newOne);
+        repository.save(oldOne);
+        return oldOne;
+    }
+
+    @DeleteMapping("/{id}")
+    public Employee update(@PathVariable Long id) {
+        Employee oldOne = repository.findById(id).orElseThrow();
+        repository.delete(oldOne);
+        return oldOne;
+    }
+
+}
